e70804adef8045f57194f0a46090a530911c181b
parusa-g <gian.parusa@psi.ch>
2025-02-11 18:28:22 +0100

    added the module and examples

---
+++ Examples/run_example.py
@@ -0,0 +1,123 @@
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import xml.etree.ElementTree as ET
+sys.path.append('../')
+sys.path.append('../../PseudoTool/')
+from Solve_schrodinger import SolveSchrodingerAtomic
+from ReadPseudoXml import readxml
+# =============================================================================
+def Plot(x,y,xlabel='',ylabel='',title='',labels=[]):
+    fig, ax = plt.subplots()
+    
+    ydim = y.ndim
+    
+    if ydim == 1:
+        ax.plot(x, y)
+    elif ydim > 1:
+        for i in range(y.shape[1]):
+            if len(labels) > 0:
+                ax.plot(x, y[:,i], label=labels[i])
+            else:
+                ax.plot(x, y[:,i])
+    
+    if len(xlabel) > 0:
+        ax.set_xlabel(xlabel)
+        
+    if len(ylabel) > 0:
+        ax.set_ylabel(ylabel)
+        
+    if len(title) > 0:
+        ax.set_title(title)
+        
+    if len(labels) > 0:
+        ax.legend()
+        
+    plt.show()
+# =============================================================================
+def main():
+    # Initialize the class
+    calc = SolveSchrodingerAtomic(grid_type='mixed', xmin=0., xmax=100.,
+                                  N=30, Np=7, Nu=10, Ng=10, rc=15.)
+    
+    # for plotting 
+    rplot = np.linspace(0.0, 15., 300)
+    
+    
+    # Example with only local potential 
+    # ---------------------------------
+    
+    # This is all-electron potential from QE within atomic units 
+    rr, Si_potential = np.loadtxt('Si_potential.dat', unpack=True)
+    
+    calc.boundPot(rr, Si_potential)
+    
+    # Compute the 3s state
+    En, vn_3s = calc.getBound(l=0, n=3)
+    print(f'Energy of 3s state: {En:.6f} Ha')
+          
+    # Compute the 3p state
+    En, vn_3p = calc.getBound(l=1, n=3)
+    print(f'Energy of 3p state: {En:.6f} Ha')
+    
+    # Compute the wave functions
+    wfn_3s = calc.getWavefunc(vn_3s, rplot)
+    wfn_3p = calc.getWavefunc(vn_3p, rplot)
+    wfn = np.column_stack((wfn_3s, wfn_3p))
+    
+    Plot(rplot, wfn, xlabel='r [Bohr]', ylabel=r'$\psi(r)$', title='Silicon all-electron', labels=['3s', '3p'])
+    
+    
+    # Example with pseudopotential
+    # ----------------------------
+    # Re-initialize the class, adjusting the xmax according to the potential data to be used (Vpsloc.dat)
+    calc = SolveSchrodingerAtomic(grid_type='mixed', xmin=0., xmax=40.,
+                                  N=30, Np=7, Nu=10, Ng=10, rc=15.)
+        
+    # Read the pseudopotential
+    tree = ET.parse('Si.xml')
+    root = tree.getroot()
+    pp = readxml(root)
+    
+    # Get necessary data from the PP file
+    Dij = pp.get_data('pp_dij')
+    
+    # Only the diagonal elements that matter within Hamann's scheme
+    Dii = np.diag(Dij)
+    
+    # Vanderbilt projectors
+    rmesh = pp.get_data('pp_rmesh')
+    pp_beta = pp.get_data('pp_beta')
+    
+    # See the pp file for this one
+    lbeta = [0, 0, 1, 1, 2, 2]
+    
+    # This is the total local potential: Vpsloc = Vloc_atom + V_H + V_xc
+    rr, Vpsloc = np.loadtxt('Si_Vpsloc.dat', unpack=True)
+    
+    calc.boundPot(rr, Vpsloc, nonloc=True, Dii=Dii, rmesh=rmesh, beta=pp_beta, lbeta=lbeta)
+    
+    # Compute the 3s state
+    # In the framework of pseudopotential, the lowest state being pseudized will be counted as 1s
+    En, vn_3s = calc.getBound(l=0, n=1)
+    print(f'Energy of 3s state: {En:.6f} Ha')
+    
+    # Compute the 3p state
+    En, vn_3p = calc.getBound(l=1, n=2)
+    print(f'Energy of 3p state: {En:.6f} Ha')
+    
+    # Compute the wave functions
+    wfn_3s = calc.getWavefunc(vn_3s, rplot)
+    wfn_3p = calc.getWavefunc(vn_3p, rplot)
+    wfn = np.column_stack((np.real(wfn_3s), np.real(wfn_3p)))
+    
+    Plot(rplot, wfn, xlabel='r [Bohr]', ylabel=r'$\psi(r)$', title='Silicon pseudopotential', labels=['3s', '3p'])
+# ===================================================================================================================
+if __name__ == '__main__':
+    main()
+
+    
+    
+    
+    
+    
---
+++ Solve_schrodinger.py
@@ -0,0 +1,381 @@
+import numpy as np
+import scipy.linalg as la
+from scipy.special import legendre as leg
+from scipy.special import eval_legendre as legval
+from scipy.interpolate import make_interp_spline
+#==============================================================================
+
+class SolveSchrodingerAtomic(object):
+    '''
+    Solves Schrodinger equation for bound-state atomic problem using 
+    finite-element method - discrete variable representation (FEM-DVR).
+    '''
+#==============================================================================
+    def __init__(self,grid_type,xmin,xmax,N=30,Np=7,Nu=10,Ng=10,rc=15.):
+        self.grid = grid_type
+        self.Ng = Ng
+        
+        # Determine the spacing
+        # -----------------------
+        if grid_type == 'linear':
+            self.Ne = N
+            self.xp = np.linspace(xmin,xmax,self.Ne+1)
+            
+        elif grid_type == 'upm':
+            self.Ne = Np * Nu
+            self.xp = self.upm(xmin,xmax,Np,Nu)
+            
+        elif grid_type == 'mixed':
+            self.Ne = Np*Nu + N
+            x1 = self.upm(xmin,rc,Np,Nu)[:-1]
+            x2 = np.linspace(rc,xmax,N+1)
+            self.xp = np.concatenate((x1,x2))
+            
+            
+        self.Xg, self.Wg = self.getLobattos()
+        self.xgrid = self.getAxis()
+        self.fw, self.bw = self.getWeights()
+        self.Kin = self.Kinetic()
+        self.nonlocB = False
+#==============================================================================
+    def upm(self,xmin,xmax,Np,Nu):
+        '''
+        Universal power-mesh.
+        '''
+        h = (xmax - xmin) / (2**Np - 1)
+        idx = np.arange(Np+1)
+        pt = xmin + h*(2**idx - 1)
+        
+        ptd = np.roll(pt,-1)
+        d = ptd[:Np] - pt[:Np]
+        nd = np.arange(Nu) / Nu
+        
+        out = nd[None,:]*d[:,None] + pt[:Np,None]
+        out = np.append(out,pt[-1])
+        
+        return out
+#==============================================================================
+    def getLobattos(self):
+        '''
+        Returns the Lobatto points and weights for the Gauss-Lobatto quadrature.
+        '''
+        LobattoPoints = leg(self.Ng).deriv(1).r
+        LobattoPoints = sorted(LobattoPoints)
+        LobattoPoints = np.insert(LobattoPoints, 0, -1.)
+        
+        # Lobatto weights
+        val = self.Ng * (self.Ng+1) * legval(self.Ng, LobattoPoints)**2
+        LobattoWeights = 2/val
+        
+        return LobattoPoints, LobattoWeights
+#==============================================================================
+    def getAxis(self):
+        xp1 = np.roll(self.xp,-1)
+        del1 = xp1[:self.Ne] - self.xp[:self.Ne]
+        del2 = xp1[:self.Ne] + self.xp[:self.Ne]
+        
+        pts = del1[:,None]*self.Xg[None,:] + del2[:,None]
+        pts = 0.5 * np.reshape(pts,-1)
+        pts = np.append(pts,self.xp[-1])
+        
+        return pts
+#==============================================================================
+    def getWeights(self):
+        xp1 = np.roll(self.xp,-1)
+        dlt = xp1[:self.Ne] - self.xp[:self.Ne]
+
+        # Forward weights
+        w1 = 0.5 * self.Wg[None,:] * dlt[:,None]
+        # to avoid divide-by-zero
+        w1 = np.append(w1,1.)
+
+        # Backward weights
+        dlt = np.insert(dlt,0,1.)
+        w2 = dlt/ self.Ng/ (self.Ng + 1)
+
+        return w1, w2
+#==============================================================================
+    def LobattoPoly(self,i,m,x):
+        '''
+        Lobatto polynomial.
+        '''
+        
+        val = 0.
+        if i>=0 and i<self.Ne-1:
+            if i!=0 or m!=0:
+                if x>=self.xgrid[i * self.Ng] and x<self.xgrid[(i+1) * self.Ng]:
+                    val = 1.
+                    for j in range(self.Ng+1):
+                        if j!=m:
+                            val *= x - self.xgrid[i*self.Ng + j]
+                            val /= self.xgrid[i*self.Ng + m] - self.xgrid[i*self.Ng + j]
+                        
+        elif i==self.Ne-1 and (m>=0 and m<=self.Ng):
+            if x>=self.xgrid[i * self.Ng]:
+                val = 1.
+                for j in range(self.Ng+1):
+                    if j!=m:
+                        val *= x - self.xgrid[i*self.Ng + j]
+                        val /= self.xgrid[i*self.Ng + m] - self.xgrid[i*self.Ng + j]
+
+        return val
+#==============================================================================
+    def LobattoBasis(self,i,m,x):
+        val = 0.
+        if m==0:
+            val = self.LobattoPoly(i-1,self.Ng,x) + self.LobattoPoly(i,0,x)
+            val /= np.sqrt(self.bw[i] + self.fw[i*self.Ng])
+            
+        elif m==self.Ng:
+            val = self.LobattoPoly(i,self.Ng,x) + self.LobattoPoly(i+1,0,x)
+            val /= np.sqrt(self.bw[i+1] + self.fw[(i+1)*self.Ng])
+            
+        elif m>0 and m<self.Ng:
+            val = self.LobattoPoly(i,m,x)
+            val /= np.sqrt(self.fw[i*self.Ng + m])
+            
+        return val
+#==============================================================================
+    def derLobatto(self,i,m1,m2):
+        '''
+        Calculating the first derivative of Lobatto polynomial.
+        '''
+        
+        val = 0.
+        if i>=0 and i<self.Ne:
+            if (i!=0 or m1!=0) and (i!=self.Ne or m1!=0):
+                if m1!=m2:
+                    val = 1.
+                    for j in range(self.Ng+1):
+                        if j!=m1 and j!=m2:
+                            val *= self.xgrid[i*self.Ng + m2] - self.xgrid[i*self.Ng + j]
+                            val /= self.xgrid[i*self.Ng + m1] - self.xgrid[i*self.Ng + j]
+                            
+                    val /= self.xgrid[i*self.Ng + m1] - self.xgrid[i*self.Ng + m2]
+                    
+                else:
+                    if m1==0:
+                        val = -1./ (2 * self.fw[i*self.Ng])
+                        
+                    elif m1==self.Ng:
+                        val = 1./ (2 * self.bw[i+1])
+                        
+        return val
+#==============================================================================
+    def derivmat(self,i):
+        fderiv = np.vectorize(self.derLobatto)
+        m1 = np.arange(self.Ng+1)
+        m2 = np.copy(m1)
+        
+        mat = fderiv(i,m1[:,None],m2[None,:])
+        
+        return mat
+#==============================================================================
+    def ttilde(self,i):
+        w = np.copy(self.fw[i*self.Ng: (i+1)*self.Ng])
+        w = np.append(w,self.bw[i+1])
+        D = self.derivmat(i)
+        
+        mat = np.einsum('ij,kj,j->ik',D,D,w)
+        
+        return mat
+#==============================================================================
+    def Kinetic(self):
+        '''
+        Calculate the kinetic energy matrix elements.
+        '''
+        n = self.Ne * self.Ng
+        T = np.zeros((n,n))
+        
+        for i in range(self.Ne):
+            tm = self.ttilde(i)
+            tmm1 = self.ttilde(i-1)
+            
+            # i1 = i2
+            # -------------------
+            
+            # m1>0, m2>0
+            t = np.copy(tm[1: self.Ng, 1: self.Ng])
+            w1 = np.copy(self.fw[i*self.Ng+1: (i+1)*self.Ng])
+            w2 = np.copy(w1)
+            w = np.sqrt(w1[:,None]*w2[None,:])
+            T[i*self.Ng+1: (i+1)*self.Ng, i*self.Ng+1: (i+1)*self.Ng] = 0.5 * t / w
+            
+            # m1=0, m2>0
+            t = np.copy(tm[0, 1: self.Ng])
+            w1 = np.copy(self.fw[i*self.Ng+1: (i+1)*self.Ng])
+            w2 = self.bw[i] + self.fw[i*self.Ng]
+            w = np.sqrt(w1*w2)
+            T[i*self.Ng, i*self.Ng+1: (i+1)*self.Ng] = 0.5 * t / w
+            
+            # m1>0, m2=0
+            t = np.copy(tm[1: self.Ng, 0])
+            T[i*self.Ng+1: (i+1)*self.Ng, i*self.Ng] = 0.5 * t / w
+            
+            # m1=m2=0
+            t = np.copy(tm[0,0] + tmm1[-1,-1])
+            w = self.bw[i] + self.fw[i*self.Ng]
+            T[i*self.Ng, i*self.Ng] = 0.5 * t / w
+            
+            # i1 = i2+1
+            # --------------------
+            
+            if i>0:
+                
+                # m1=0, m2>0
+                t = np.copy(tmm1[-1, 1: self.Ng])
+                w1 = np.copy(self.fw[(i-1)*self.Ng+1: i*self.Ng])
+                w2 = np.copy(self.bw[i] + self.fw[i*self.Ng])
+                w = np.sqrt(w1*w2)
+                T[i*self.Ng, (i-1)*self.Ng+1: i*self.Ng] = 0.5 * t / w
+                
+                # m1=m2=0
+                t = np.copy(tmm1[-1,0])
+                w1 = np.copy(self.bw[i] + self.fw[i*self.Ng])
+                w2 = np.copy(self.bw[i-1] + self.fw[(i-1)*self.Ng])
+                w = np.sqrt(w1*w2)
+                T[i*self.Ng, (i-1)*self.Ng] = 0.5 * t / w
+                
+            
+            # i1 = i2-1
+            # --------------------
+            
+            if i<self.Ne-1:
+                
+                # m1>0, m2=0
+                t = np.copy(tm[1:self.Ng, -1])
+                w1 = np.copy(self.fw[i*self.Ng+1: (i+1)*self.Ng])
+                w2 = np.copy(self.bw[i+1] + self.fw[(i+1)*self.Ng])
+                w = np.sqrt(w1*w2)
+                T[i*self.Ng+1: (i+1)*self.Ng, (i+1)*self.Ng] = 0.5 * t / w
+                
+                # m1=m2=0
+                t = np.copy(tm[0,-1])
+                w1 = np.copy(self.bw[i] + self.fw[i*self.Ng])
+                w2 = np.copy(self.bw[i+1] + self.fw[(i+1)*self.Ng])
+                w = np.sqrt(w1*w2)
+                T[i*self.Ng, (i+1)*self.Ng] = 0.5 * t / w
+                
+        return T[1:, 1:]
+#==============================================================================
+    def init_nonloc(self,D,rmesh,beta,lbeta):
+        '''
+        Initialize variables for using a non-local potential.
+        '''
+        self.D = D
+        self.rmesh = rmesh
+        self.beta = beta
+        self.lbeta = lbeta
+#==============================================================================
+    def get_Vnl(self,l):
+        '''
+        Calculate the non-local potential matrix elements.
+        '''
+        nproj = self.beta.shape[-1]
+        N = len(self.xgrid[1:-1])
+        chjj = np.zeros((N,N), dtype=complex)
+        
+        for i in range(nproj):
+            if self.lbeta[i] == l:
+                splr = make_interp_spline(self.rmesh, np.real(self.beta[:,i]))
+                spli = make_interp_spline(self.rmesh, np.imag(self.beta[:,i]))
+
+                beta_r = splr(self.xgrid[1:-1])
+                ch_re = [beta_r for i in range(N)]
+                ch_re = np.array(ch_re)
+
+                beta_i = spli(self.xgrid[1:-1])
+                ch_im = [beta_i for i in range(N)]
+                ch_im = np.array(ch_im)
+
+                chj = ch_re + 1j*ch_im
+                chjj += self.D[i] * chj * np.conj(chj.T)
+
+        w = np.copy(self.fw)
+        w = np.sqrt(w)
+        for i in range(self.Ne):
+            w[i*self.Ng] = np.sqrt(self.bw[i] + self.fw[i*self.Ng])
+
+        wj = [w[1:-1] for i in range(N)]
+        wj = np.array(wj)
+        wjj = wj * wj.T
+        
+        if wjj.shape != chjj.shape:
+            raise ValueError('Something wrong with the dimension of the \
+                              weight and/or projector matrices. ')
+
+        Vnl = wjj * chjj
+
+        return Vnl
+#==============================================================================
+    def boundPot(self,rr,Vloc,nonloc=False,Dii=None,rmesh=None,beta=None,lbeta=None):
+        '''
+        Initialize the bound-state potential.
+        rr     : radial grid for Vloc
+        Vloc   : local potential
+        nonloc : True/False, whether to include non-local potential
+        Dii    : Vanderbilt projector eigenvalues
+        rmesh  : radial grid for the Vanderbilt projector
+        beta   : Vanderbilt projectors
+        lbeta  : angular momentum quantum number for the projectors
+        '''
+        
+        # Use the effective Z value
+        self.Z = rr[-1] * Vloc[-1]
+        
+        self.Vrb = make_interp_spline(rr, Vloc, k=3)
+
+        if nonloc == True:
+            self.nonlocB = True
+            self.init_nonloc(Dii, rmesh, beta, lbeta)
+#==============================================================================
+    def Vlr(self,l):
+        Vr = self.Vrb(self.xgrid[1:-1])
+        Vl = 0.5 * l*(l+1) / self.xgrid[1:-1]**2
+        Vlr = Vr + Vl
+        
+        return Vlr 
+#==============================================================================
+    def getBound(self,l,n=None):
+        Vlr = np.diag(self.Vlr(l))
+        Ham = self.Kin + Vlr
+        
+        if self.nonlocB == True:
+            Ham = Ham + self.get_Vnl(l)
+
+        E, V = la.eigh(Ham)
+        
+        if n != None:
+            k = n - l - 1
+            if k >= 0:
+                E = E[k]
+                V = V[:,k]
+            else: 
+                raise ValueError('wrong n value')
+        
+        return E, V
+#==============================================================================
+    def Psi(self,eigv,x):
+        '''
+        Calculate the wave function at a given point x.
+        '''
+        
+        chi = [[self.LobattoBasis(i,m,x) for m in range(self.Ng)] for i in range(self.Ne)]
+        chi = np.array(chi).reshape(-1)
+
+        psi = np.dot(eigv, chi[1:])
+
+        return psi
+#==============================================================================
+    def getWavefunc(self,eigv,rr):
+        '''
+        Calculate the wave function on a given radial grid.
+        '''
+        
+        psi = [self.Psi(eigv,x) for x in rr]
+        psi = np.array(psi)
+
+        return psi
+#==============================================================================
+    
+    

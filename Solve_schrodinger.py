fd3f69d200824622fc12ecc1abc12ee7dc7be34d
parusa-g <gian.parusa@gmail.com>
2025-07-10 18:46:06 +0200

    Added features for solving scattering problem

--- Solve_schrodinger.py
+++ Solve_schrodinger.py
@@ -3,14 +3,16 @@ import numpy as np
 import scipy.linalg as la
 from scipy.special import legendre as leg
 from scipy.special import eval_legendre as legval
+from scipy.special import gamma
 from scipy.interpolate import make_interp_spline
 sys.path.append('../PseudoTool/')
 from ReadPseudoXml import parse
+from CoulombWF import coul90
 #==============================================================================
 class SolveSchrodingerAtomic(object):
     '''
-    Solves Schrodinger equation for bound-state atomic problem using 
-    finite-element method - discrete variable representation (FEM-DVR).
+    Solves Schrodinger equation for bound-state and scattering-state atomic problem 
+    using finite-element method - discrete variable representation (FEM-DVR).
     '''
 #==============================================================================
     def __init__(self,grid_type,xmin,xmax,N=30,Np=7,Nu=10,Ng=10,rc=15.,pp_xml=''):
@@ -39,9 +41,10 @@ class SolveSchrodingerAtomic(object):
         self.fw, self.bw = self.getWeights()
         self.Kin = self.Kinetic()
         
-        self.nonlocB = False
+        self.nonloc = False
+           
         if len(pp_xml) > 0:
-            self.nonlocB = True
+            self.nonloc = True
             
             pp = parse(pp_xml)
             Dij = pp.get_data('pp_dij')
@@ -58,16 +61,16 @@ class SolveSchrodingerAtomic(object):
         '''
         h = (xmax - xmin) / (2**Np - 1)
         idx = np.arange(Np+1)
-        pt = xmin + h*(2**idx - 1)
+        Points = xmin + h*(2**idx - 1)
         
-        ptd = np.roll(pt,-1)
-        d = ptd[:Np] - pt[:Np]
-        nd = np.arange(Nu) / Nu
+        NextPoints = np.roll(Points,-1)
+        Delta = NextPoints[:Np] - Points[:Np]
+        Nd = np.arange(Nu) / Nu
         
-        out = nd[None,:]*d[:,None] + pt[:Np,None]
-        out = np.append(out,pt[-1])
+        FinalPoints = Nd[None,:]*Delta[:,None] + Points[:Np,None]
+        FinalPoints = np.append(FinalPoints,Points[-1])
         
-        return out
+        return FinalPoints
 #==============================================================================
     def getLobattos(self):
         '''
@@ -194,6 +197,124 @@ class SolveSchrodingerAtomic(object):
         
         return mat
 #==============================================================================
+    def tderiv1(self,i):
+        wp = np.copy(self.fw[i*self.Ng: (i+1)*self.Ng])
+        wp = np.append(wp,self.bw[i+1])
+        w = np.array( [wp for i in range(self.Ng+1)] )
+        D = self.derivmat(i)
+
+        return D.T * w.T
+#==============================================================================
+    def FmdvrBoundary(self):
+        v = np.zeros(self.Ne * self.Ng + 1)
+        tm = self.ttilde(self.Ne - 1)
+        
+        # i=ne-1, m=0
+        t = np.copy(tm[0,-1])
+        w1 = np.copy(self.bw[-1])
+        w2 = np.copy(self.bw[-2] + self.fw[(self.Ne-1) * self.Ng])
+        w = np.sqrt(w1*w2)
+        v[(self.Ne-1) * self.Ng] = 0.5 * t / w
+        
+        # i=ne-1, m>0
+        t = np.copy(tm[1:self.Ng, -1])
+        w1 = np.copy(self.fw[(self.Ne-1)*self.Ng+1: -1])
+        w2 = np.copy(self.bw[-1])
+        w = np.sqrt(w1*w2)
+        v[(self.Ne-1)*self.Ng+1: -1] = 0.5 * t / w
+        
+        return v[1:-1]
+#==============================================================================
+    def ProjectOntoLobattoBasis(self,spl):
+        ' Calculate the projection onto the FEMDVR basis.     '
+        ' spl: interpolator for the function to be projected. '
+
+        w = np.copy(self.fw)
+        w = np.sqrt(w)
+        for i in range(self.Ne):
+            w[i*self.Ng] = np.sqrt(self.bw[i] + self.fw[i*self.Ng])
+            
+        proj = spl(self.xgrid[1:-1]) * w[1:-1]
+
+        return proj
+#==============================================================================
+    def GetFirstDeriv(self):
+        '''
+        The first derivative operator in the Lobatto basis.
+        '''
+        n = self.Ne * self.Ng
+        T = np.zeros((n,n))
+
+        for i in range(self.Ne):
+            tm = self.tderiv1(i)
+            tmm1 = self.tderiv1(i-1)
+
+            # i1 = i2
+            # --------------
+
+            # m1>0, m2>0
+            t = np.copy(tm[1: self.Ng, 1: self.Ng])
+            w1 = np.copy(self.fw[i*self.Ng+1: (i+1)*self.Ng])
+            w2 = np.copy(w1)
+            w = np.sqrt(w1[:,None]*w2[None,:])
+            T[i*self.Ng+1: (i+1)*self.Ng, i*self.Ng+1: (i+1)*self.Ng] = t / w
+
+            # m1=0, m2>0
+            t = np.copy(tm[0, 1: self.Ng])
+            w1 = np.copy(self.fw[i*self.Ng+1: (i+1)*self.Ng])
+            w2 = self.bw[i] + self.fw[i*self.Ng]
+            w = np.sqrt(w1*w2)
+            T[i*self.Ng, i*self.Ng+1: (i+1)*self.Ng] = t / w
+
+            # m1>0, m2=0
+            t = np.copy(tm[1: self.Ng, 0])
+            T[i*self.Ng+1: (i+1)*self.Ng, i*self.Ng] = t / w
+
+            # m1=m2=0
+            t = np.copy(tm[0,0] + tmm1[-1,-1])
+            w = self.bw[i] + self.fw[i*self.Ng]
+            T[i*self.Ng, i*self.Ng] = t / w
+
+            # i1 = i2+1
+            # --------------------
+
+            if i>0:
+
+                # m1=0, m2>0
+                t = np.copy(tmm1[-1, 1: self.Ng])
+                w1 = np.copy(self.fw[(i-1)*self.Ng+1: i*self.Ng])
+                w2 = np.copy(self.bw[i] + self.fw[i*self.Ng])
+                w = np.sqrt(w1*w2)
+                T[i*self.Ng, (i-1)*self.Ng+1: i*self.Ng] = t / w
+                
+                # m1=m2=0
+                t = np.copy(tmm1[-1, 0])
+                w1 = np.copy(self.bw[i] + self.fw[i*self.Ng])
+                w2 = np.copy(self.bw[i-1] + self.fw[(i-1)*self.Ng])
+                w = np.sqrt(w1*w2)
+                T[i*self.Ng, (i-1)*self.Ng] = t / w
+
+            # i1 = i2-1
+            # --------------------
+            
+            if i<self.Ne-1:
+                
+                # m1>0, m2=0
+                t = np.copy(tm[1:self.Ng, -1])
+                w1 = np.copy(self.fw[i*self.Ng+1: (i+1)*self.Ng])
+                w2 = np.copy(self.bw[i+1] + self.fw[(i+1)*self.Ng])
+                w = np.sqrt(w1*w2)
+                T[i*self.Ng+1: (i+1)*self.Ng, (i+1)*self.Ng] = t / w
+                
+                # m1=m2=0
+                t = np.copy(tm[0,-1])
+                w1 = np.copy(self.bw[i] + self.fw[i*self.Ng])
+                w2 = np.copy(self.bw[i+1] + self.fw[(i+1)*self.Ng])
+                w = np.sqrt(w1*w2)
+                T[i*self.Ng, (i+1)*self.Ng] = t / w
+
+        return T[1:, 1: ]
+#==============================================================================
     def Kinetic(self):
         '''
         Calculate the kinetic energy matrix elements.
@@ -279,6 +400,7 @@ class SolveSchrodingerAtomic(object):
         self.D = D
         self.rmesh = rmesh
         self.beta = beta
+        self.beta_is_complex = np.iscomplexobj(beta)
         self.lbeta = lbeta
 #==============================================================================
     def get_Vnl(self,l):
@@ -287,62 +409,117 @@ class SolveSchrodingerAtomic(object):
         '''
         nproj = self.beta.shape[-1]
         N = len(self.xgrid[1:-1])
-        chjj = np.zeros((N,N), dtype=complex)
+        
+        if self.beta_is_complex:
+            Vnl = np.zeros((N,N), dtype=complex)
+        else:
+            Vnl = np.zeros((N,N))
         
         for i in range(nproj):
             if self.lbeta[i] == l:
-                splr = make_interp_spline(self.rmesh, np.real(self.beta[:,i]))
-                spli = make_interp_spline(self.rmesh, np.imag(self.beta[:,i]))
+                spl_r = make_interp_spline(self.rmesh, np.real(self.beta[:,i]))
+                beta = self.ProjectOntoLobattoBasis(spl_r)
+                
+                if self.beta_is_complex:
+                    spl_i = make_interp_spline(self.rmesh, np.imag(self.beta[:,i]))
+                    beta = beta + 1j*self.ProjectOntoLobattoBasis(spl_i)
 
-                beta_r = splr(self.xgrid[1:-1])
-                ch_re = [beta_r for i in range(N)]
-                ch_re = np.array(ch_re)
-
-                beta_i = spli(self.xgrid[1:-1])
-                ch_im = [beta_i for i in range(N)]
-                ch_im = np.array(ch_im)
-
-                chj = ch_re + 1j*ch_im
-                chjj += self.D[i] * chj * np.conj(chj.T)
-
-        w = np.copy(self.fw)
-        w = np.sqrt(w)
-        for i in range(self.Ne):
-            w[i*self.Ng] = np.sqrt(self.bw[i] + self.fw[i*self.Ng])
-
-        wj = [w[1:-1] for i in range(N)]
-        wj = np.array(wj)
-        wjj = wj * wj.T
-        
-        if wjj.shape != chjj.shape:
-            raise ValueError('Something wrong with the dimension of the \
-                              weight and/or projector matrices. ')
-
-        Vnl = wjj * chjj
+                Vnl += self.D[i] * np.outer(beta, np.conj(beta))
 
         return Vnl
 #==============================================================================
-    def boundPot(self,rr,Vloc):
+    def InitPotential(self,rr,Vloc):
         '''
         Initialize the bound-state potential.
         rr     : radial grid for Vloc
         Vloc   : local potential
         '''
-    
-        self.Vrb = make_interp_spline(rr, Vloc, k=3)
+        self.Z = rr[-1] * Vloc[-1]
+        self.Vr = make_interp_spline(rr, Vloc, k=3)
+#==============================================================================
+    def InitScattering(self,E):
+        '''
+        Initialize the scattering state parameters.
+        E      : energy of the scattering state
+        '''
+        self.E = E
+        self.ke = np.sqrt(2. * E)
+        self.eta = self.Z / self.ke
+#==============================================================================
+    def CoulombFunction(self,l,r,real=False):
+        '''
+        Calculate the Coulomb functions
+        l       : angular momentum channel
+        r       : radial point
+        real    : real or complex Coulomb function
+        '''
+        
+        Fc, Gc, _, _, _ = coul90(self.ke*r,self.eta,l+1,0)
+        F = Fc[l]
+        G = Gc[l]
+        f = np.angle(gamma(l+1 + 1j*self.eta))
+        r = np.exp(-1j * f)
+            
+        if real:
+            Hp = F
+            Hm = G
+        else:
+            Hp = r * (F + 1.0j*G)
+            Hm = np.conj(Hp)
+        
+        return Hp, Hm
+#==============================================================================    
+    def GetCoulombBoundaryCondition(self,l,real):
+        '''
+        Calculate the Coulomb boundary condition in the FEM-DVR basis:
+        l       : angular momentum channel
+        real    : real or complex Coulomb functions
+        '''
+        r1 = self.xgrid[-1]
+        r2 = self.xgrid[-2]
+        H1 = self.CoulombFunction(l,r1,real)
+        H2 = self.CoulombFunction(l,r2,real)
+        
+        a = H1[1] / H2[1]        
+        b = H1[0] - a*H2[0]
+        
+        a *= np.sqrt(self.bw[-1] / self.fw[-2])
+        b *= np.sqrt(self.bw[-1])
+        
+        return a, b
+#==============================================================================
+    def AddScatteringBoundary(self,x,l,const,real):
+        '''
+        Add the scattering boundary condition to the wave function.
+        x       : the grid point
+        l       : angular momentum channel 
+        const   : phase factor between the Coulomb functions
+        real    : real or complex Coulomb functions
+        '''
+        R = self.GetCoulombBoundaryCondition(l,real)
+        c = R[0]*const + R[1]
+        
+        val = c * self.lobatto(self.Ne-1,self.Ng,x) / np.sqrt(self.bw[-1])
+        
+        return val
 #==============================================================================
     def Vlr(self,l):
-        Vr = self.Vrb(self.xgrid[1:-1])
+        Vr = self.Vr(self.xgrid[1:-1])
         Vl = 0.5 * l*(l+1) / self.xgrid[1:-1]**2
         Vlr = Vr + Vl
         
         return Vlr 
 #==============================================================================
-    def getBound(self,l,n=None):
+    def GetBound(self,l,n=None):
+        '''
+        Solves the bound-state problem.
+        l   : angular momentum quantum number
+        n   : principal quantum number
+        '''
         Vlr = np.diag(self.Vlr(l))
         Ham = self.Kin + Vlr
         
-        if self.nonlocB == True:
+        if self.nonloc == True:
             Ham = Ham + self.get_Vnl(l)
 
         E, V = la.eigh(Ham)
@@ -357,29 +534,150 @@ class SolveSchrodingerAtomic(object):
         
         return E, V
 #==============================================================================
-    def Psi(self,eigv,x):
+    def GetHamMinEn(self,l,c):
+        Em = self.E * np.eye(self.Ne*self.Ng - 1)
+        Vlr = np.diag(self.Vlr(l))
+        v = self.FmdvrBoundary()
+        
+        mat = self.Kin + Vlr - Em
+            
+        if self.nonloc == True:
+            mat = mat + self.get_Vnl(l)
+            
+        if np.iscomplexobj(c):
+            mat = mat.astype(complex)
+            
+        mat[:, -1] = np.copy(mat[:, -1] + c*v)
+        
+        return mat
+#==============================================================================    
+    def GetScatt(self,E,l,real=False):
+        '''
+        Solves the scattering state.
+        E       : scattering energy [Ha]
+        l       : angular momentum quantum number
+        real    : real or complex Coulomb function as the boundary condition
+        '''
+        self.InitScattering(E)
+        
+        K = self.GetCoulombBoundaryCondition(l,real)
+        v = self.FmdvrBoundary()
+        A = self.GetHamMinEn(l,K[0])
+        b = -K[1] * v
+        
+        x = la.solve(A,b)
+        
+        return x
+#==============================================================================
+    def Psix(self,eigv,x,bound=True,l=0,real=False):
         '''
         Calculate the wave function at a given point x.
+        eigv    : eigenvalue of the Hamiltonian
+        x       : the grid point
+        bound   : whether to calculate the bound-state or scattering state
+        l       : if bound=True, angular momentum channel
+        real    : if bound=True, real or complex Coulomb function as the boundary condition
         '''
         
-        chi = [[self.LobattoBasis(i,m,x) for m in range(self.Ng)] for i in range(self.Ne)]
-        chi = np.array(chi).reshape(-1)
+        basis = [[self.LobattoBasis(i,m,x) for m in range(self.Ng)] for i in range(self.Ne)]
+        basis = np.array(basis).reshape(-1)
 
-        psi = np.dot(eigv, chi[1:])
+        psi = np.dot(eigv, basis[1:])
+        
+        if not bound:
+            psi += self.AddScatteringBoundary(x,l,eigv[-1],real)
 
         return psi
 #==============================================================================
-    def getWavefunc(self,eigv,rr):
+    def GetWavefunc(self,eigv,rr,bound=True,l=0,real=False):
         '''
         Calculate the wave function on a given radial grid.
+        eigv    : eigenvalue of the Hamiltonian
+        rr      : array of grid points
         '''
         
-        psi = [self.Psi(eigv,x) for x in rr]
+        psi = [self.Psix(eigv,x,bound,l,real) for x in rr]
         psi = np.array(psi)
         
-        psi = np.real(psi)
-        irmax = np.argmax(np.abs(psi))
-        psi *= np.sign(psi[irmax])
+        if bound:
+            irmax = np.argmax(np.abs(psi))
+            psi *= np.sign(psi[irmax])
 
         return psi
+#==============================================================================
+    def GetScatteringPhase(self,KinE,l,real=False):
+        '''
+        Calculate the scattering phase at infinity.
+        KinE    : kinetic (scattering) energy [Ha]
+        l       : angular momentum channel
+        '''
+        
+        # Infinity is taken to be the last point in the grid
+        R = self.xgrid[-1]
+        phase = []
+        
+        if real:
+            f = np.angle(gamma(l+1 + 1j*self.eta))
+            f = np.exp(-2j * f)
+        
+        for E in KinE:
+            v = self.GetScatt(E,l,real)
+            yR = self.Psix(v,R,l,bound=False,real=real)
+            H = self.CoulombFunction(l,R,real)
+
+            S = (yR - H[0]) / H[1]
+
+            if real:
+                S = (1. + 1.0j*S) / (1. - 1.0j*S)
+                S *= f
+
+            phase.append(S)
+
+        Sl = np.array(phase)
+
+        return np.angle(Sl) / np.pi
+#==============================================================================
+    def GetRadialIntegral(self,KinE,n,l,verbose='low',Boundwfc=[]):
+        '''
+        Calculate the photo-emission radial integral.
+        KinE     : kinetic (scattering) energy [Ha]
+        n        : principal quantum number
+        l        : angular momentum channel
+        verbose  : verbosity level ('low', 'high')
+        Boundwfc : bound-state wave function for the radial integral
+        '''
+        
+        if len(Boundwfc) == 0:
+            _, b = self.GetBound(l, n)
+            psi_inf = self.Psix(b, self.xgrid[-1])
+            
+            if psi_inf < 0.0:
+                b *= -1.0
+        else:
+            bound_spl = make_interp_spline(Boundwfc[:,0], Boundwfc[:,1])
+            b = self.ProjectOntoLobattoBasis(bound_spl)
+            
+        rb = self.xgrid[1:-1] * b
+        
+        if l == 0:
+            l_final = [1]
+        else:
+            l_final = [l-1, l+1]
+            
+        radint = np.zeros((len(KinE), len(l_final)), dtype=complex)
+        
+        for i,l1 in enumerate(l_final):
+            
+            print('Calculating for scattering channel l =' + str(l1))
+            
+            for j,E in enumerate(KinE):
+                if verbose == 'high':
+                    print('Calculating for energy E = ' + str(E) + ' Ha')
+                    
+                v = self.GetScatt(E, l1)
+                dot = np.dot(np.conj(v), rb)
+                
+                radint[j,i] = dot
+                
+        return radint
 #==============================================================================
